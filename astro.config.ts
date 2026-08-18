03437a4ddf78e78629ebc643c470e22c80613175
Ricardo Perez <richartl@Ricardos-MacBook-Pro-2.local>
2026-08-18 15:54:15 -0600

    add workflow

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://richartl.github.io',
+  base: '/personal-blog',
   trailingSlash: 'ignore',
   integrations: [sitemap()],
   vite: {
